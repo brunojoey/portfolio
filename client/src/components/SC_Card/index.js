5b6e3e7e45edfdede56b694ad7c90b749c8b4858
Joey Bruno <brunojoey12@gmail.com>
2020-10-20 10:18:53 -0500

    Took out instances of SC_PDF

--- client/src/components/SC_Card/index.js
+++ client/src/components/SC_Card/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import SocialMediaMap from '../SC_Map';
-// import SocialMediaPDF from '../SC_PDF';
 
 function SocialMediaCard() {
     return (
         <>
-            {/* <SocialMediaPDF /> */}
             <SocialMediaMap />
         </>
     )
